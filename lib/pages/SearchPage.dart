edde2ae8f37d14e4f1b70d21a16eaba811b5b3c0
Akash Yadav <aacyadav4440@gmail.com>
2022-07-06 07:21:45 +0530

    Search Page Completed

---
+++ lib/pages/SearchPage.dart
@@ -0,0 +1,75 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:watch_me_travel/utils/colors.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final TextEditingController searchController = TextEditingController();
+  bool isShowUsers = false;
+
+  @override
+  void dispose() {
+    super.dispose();
+    searchController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: TextFormField(
+          controller: searchController,
+          decoration: const InputDecoration(
+            labelText: 'Search for a user',
+          ),
+          onFieldSubmitted: (String _) {
+            setState(() {
+              isShowUsers = true;
+            });
+          },
+        ),
+      ),
+      body: isShowUsers
+          ? FutureBuilder(
+              future: FirebaseFirestore.instance
+                  .collection('users')
+                  .where(
+                    'username',
+                    isGreaterThanOrEqualTo: searchController.text,
+                  )
+                  .get(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return ListView.builder(
+                  itemCount: (snapshot.data! as dynamic).docs.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            (snapshot.data! as dynamic).docs[index]
+                                ['photoURL']),
+                      ),
+                      title: Text(
+                          (snapshot.data! as dynamic).docs[index]['username']),
+                    );
+                  },
+                );
+              },
+            )
+          : Text('Post')//Image(
+              //image: NetworkImage(
+                  //'https://i.pinimg.com/originals/f5/e8/a5/f5e8a551428b98918c1d18d438cfbcfd.jpg')),
+    );
+  }
+}
--- lib/utils/GlobalVaribles.dart
+++ lib/utils/GlobalVaribles.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:watch_me_travel/pages/AddPost.dart';
 import 'package:watch_me_travel/pages/FeedPage.dart';
+import 'package:watch_me_travel/pages/ProfilePage.dart';
+import 'package:watch_me_travel/pages/SearchPage.dart';
 
 const webSizeScreen = 600;
 
 const homeScreenItems = [
   FeedPage(),
-  Center(child: Text('Search')),
+  SearchPage(),
   AddPostScreen(),
   Center(child: Text('Likes')),
-  Center(child: Text('Profile')),
+  ProfilePage(),
 ];
